9c1369a053cffd83a9a59d86942bb9ceb6a572ab
Michael <michaellimisi7@gmail.com>
2023-03-19 00:51:03 +0300

    completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function divide(num1, num2){
+    return num1/num2
+}
+divide(2000, 100);
+
+const square = num1 => console.log(num1*num1)
+
+square(2);
+
+let add = (a, b) =>console.log(a + b)
+add(2,4);
+  
